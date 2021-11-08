274e36d43572e332602dd1141b4bd2cad73ed809
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-11-08 11:50:51 +0100

    fastapi v0.65.2

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
     packages=["square_skill_api"],
     install_requires=[
-        "uvicorn==0.15.0",
-        "fastapi==0.65.1",                     
-        "pydantic==1.8.2",                     
+        "uvicorn>=0.15.0",
+        "fastapi>=0.65.2",                     
+        "pydantic>=1.8.2",                     
     ],
 )
