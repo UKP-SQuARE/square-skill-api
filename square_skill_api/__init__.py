f1cd99999e6b00a10d9f0b6e848449c74ed63b39
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-12-20 08:00:20 +0100

    add CORS

--- square_skill_api/__init__.py
+++ square_skill_api/__init__.py
@@ -4,6 +4,7 @@ from logging.config import fileConfig
 from typing import Callable
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import square_skill_api.api.routes.query
 from square_skill_api.api.routes.router import api_router
@@ -17,6 +18,7 @@ def get_app(
     app_name: str = "square-skill-api",
     api_prefix: str = "",
     version: str = "0.1.0",
+    add_cors: bool = True
 ) -> FastAPI:
     try:
         fileConfig(
@@ -27,15 +29,24 @@ def get_app(
         logger.info(
             "Failed to load 'logging.conf'. Continuing without configuring the server logger"
         )
-    fast_app = FastAPI(title=app_name, version=version)
-    fast_app.include_router(api_router, prefix=api_prefix)
+    app = FastAPI(title=app_name, version=version)
+    app.include_router(api_router, prefix=api_prefix)
 
     # Nothing done here currently.
-    fast_app.add_event_handler("startup", start_app_handler(fast_app))
-    fast_app.add_event_handler("shutdown", stop_app_handler(fast_app))
+    app.add_event_handler("startup", start_app_handler(app))
+    app.add_event_handler("shutdown", stop_app_handler(app))
 
-    fast_app.dependency_overrides[
+    app.dependency_overrides[
         square_skill_api.api.routes.query.predict
     ] = lambda: predict_fn
 
-    return fast_app
+    if add_cors:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+    return app
