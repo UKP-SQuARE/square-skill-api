7a57e2c6dae3066641cfc951362ccebfb10eff30
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-02-23 14:28:11 +0100

    fix square-auth install

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="square_skill_api",
-    version="0.0.10",
+    version="0.0.11",
     description="",
     url="www.informatik.tu-darmstadt.de/ukp",
     author="UKP",
     author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
     packages=find_packages(exclude=("tests", ".gitignore", "pytest.ini", "requirements.dev.txt")),
-    dependency_links=['https://github.com/UKP-SQuARE/square-auth/tarball/v0.0.2#egg=square-auth-0.0.2'],
     install_requires=[
-        "square-auth==0.0.2",
+        "square-auth>=0.0.3",
         "uvicorn>=0.15.0",
         "fastapi>=0.65.2",
         "pydantic>=1.8.2",
