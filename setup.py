dc9669fabaf223c2da8a340b5e9a9047a1633c7b
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-02 17:41:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="square_skill_api",
-    version="0.0.12",
+    version="0.0.14",
     description="",
     url="www.informatik.tu-darmstadt.de/ukp",
     author="UKP",
