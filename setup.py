4f781318087a271ef326870fabc605087070be29
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-13 16:52:47 +0100

    update to version 0.0.17

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="square_skill_api",
-    version="0.0.16",
+    version="0.0.17",
     description="",
     url="www.informatik.tu-darmstadt.de/ukp",
     author="UKP",
     author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
     packages=find_packages(exclude=("tests", ".gitignore", "pytest.ini", "requirements.dev.txt")),
     install_requires=[
-        "square-auth>=0.0.3",
         "uvicorn>=0.15.0",
         "fastapi>=0.65.2",
         "pydantic>=1.8.2",
