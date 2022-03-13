4f40238d43602470f6d3c6c4d3e79e68eefb7e7f
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-13 16:58:15 +0100

    add Predeiction models to init

--- square_skill_api/models/__init__.py
+++ square_skill_api/models/__init__.py
@@ -1,3 +1,8 @@
 from square_skill_api.models.heartbeat import HeartbeatResult
-from square_skill_api.models.prediction import QueryOutput
+from square_skill_api.models.prediction import (
+    QueryOutput,
+    Prediction,
+    PredictionDocument,
+    PredictionOutput,
+)
 from square_skill_api.models.request import QueryRequest
